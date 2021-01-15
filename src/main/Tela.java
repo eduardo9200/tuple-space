eacca53fdfd0558ca81e433aea28cb569d00511d
eduardo9200 <edumaia92@gmail.com>
2021-01-15 16:55:17 -0300

    Adição de novas ações a alguns dos botões da tela e criação de JLabel para pequenos logs ao usuário.

--- src/main/Tela.java
+++ src/main/Tela.java
@@ -5,25 +5,38 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import commons.Service;
+import net.jini.core.entry.UnusableEntryException;
+import net.jini.core.transaction.TransactionException;
+import net.jini.space.JavaSpace;
+
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import java.awt.event.ActionListener;
+import java.rmi.RemoteException;
+import java.util.Scanner;
 import java.awt.event.ActionEvent;
+import java.awt.Color;
+import java.awt.Font;
 
 public class Tela extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	
+	private JavaSpace space;
 	
 	private JPanel contentPane;
 	
 	private JLabel lblInserir;
 	private JLabel lblRemover;
 	private JLabel lblMover;
+	private JLabel lblLog;
 	
-	private JButton btnNewCloud;
+	private JButton btnNewNuvem;
 	private JButton btnNewHost;
 	private JButton btnNewVM;
 	private JButton btnNewProcesso;
@@ -42,16 +55,18 @@ public class Tela extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
+		Tela frame = new Tela();
+		frame.setVisible(true);
+		frame.instanciaEspacoTupla();
+		/*EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Tela frame = new Tela();
-					frame.setVisible(true);
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
-		});
+		});*/
 	}
 
 	/**
@@ -59,7 +74,7 @@ public class Tela extends JFrame {
 	 */
 	public Tela() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 500);
+		setBounds(100, 100, 450, 525);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -70,73 +85,95 @@ public class Tela extends JFrame {
 	}
 	
 	private void initComponents() {
-		btnNewCloud = new JButton("Nuvem");
-		btnNewCloud.setBounds(46, 67, 89, 23);
-		contentPane.add(btnNewCloud);
+		btnNewNuvem = new JButton("Nuvem");
+		btnNewNuvem.setEnabled(false);
+		btnNewNuvem.setBounds(44, 92, 89, 23);
+		contentPane.add(btnNewNuvem);
 		
 		btnNewHost = new JButton("Host");
-		btnNewHost.setBounds(46, 101, 89, 23);
+		btnNewHost.setEnabled(false);
+		btnNewHost.setBounds(44, 126, 89, 23);
 		contentPane.add(btnNewHost);
 		
 		btnNewVM = new JButton("VM");
-		btnNewVM.setBounds(46, 135, 89, 23);
+		btnNewVM.setEnabled(false);
+		btnNewVM.setBounds(44, 160, 89, 23);
 		contentPane.add(btnNewVM);
 		
 		btnNewProcesso = new JButton("Processo");
-		btnNewProcesso.setBounds(46, 169, 89, 23);
+		btnNewProcesso.setEnabled(false);
+		btnNewProcesso.setBounds(44, 194, 89, 23);
 		contentPane.add(btnNewProcesso);
 		
 		lblInserir = new JLabel("Inserir");
-		lblInserir.setBounds(46, 42, 46, 14);
+		lblInserir.setBounds(44, 67, 46, 14);
 		contentPane.add(lblInserir);
 		
 		lblRemover = new JLabel("Remover");
-		lblRemover.setBounds(171, 42, 61, 14);
+		lblRemover.setBounds(169, 67, 61, 14);
 		contentPane.add(lblRemover);
 		
 		btnRemoveNuvem = new JButton("Nuvem");
-		btnRemoveNuvem.setBounds(171, 67, 89, 23);
+		btnRemoveNuvem.setEnabled(false);
+		btnRemoveNuvem.setBounds(169, 92, 89, 23);
 		contentPane.add(btnRemoveNuvem);
 		
 		btnRemoveHost = new JButton("Host");
-		btnRemoveHost.setBounds(171, 101, 89, 23);
+		btnRemoveHost.setEnabled(false);
+		btnRemoveHost.setBounds(169, 126, 89, 23);
 		contentPane.add(btnRemoveHost);
 		
 		btnRemoveVM = new JButton("VM");
-		btnRemoveVM.setBounds(171, 135, 89, 23);
+		btnRemoveVM.setEnabled(false);
+		btnRemoveVM.setBounds(169, 160, 89, 23);
 		contentPane.add(btnRemoveVM);
 		
 		btnRemoveProcesso = new JButton("Processo");
-		btnRemoveProcesso.setBounds(171, 169, 89, 23);
+		btnRemoveProcesso.setEnabled(false);
+		btnRemoveProcesso.setBounds(169, 194, 89, 23);
 		contentPane.add(btnRemoveProcesso);
 		
 		lblMover = new JLabel("Mover");
-		lblMover.setBounds(294, 42, 46, 14);
+		lblMover.setBounds(292, 67, 46, 14);
 		contentPane.add(lblMover);
 		
 		btnMoverHost = new JButton("Host");
-		btnMoverHost.setBounds(294, 82, 89, 23);
+		btnMoverHost.setEnabled(false);
+		btnMoverHost.setBounds(292, 107, 89, 23);
 		contentPane.add(btnMoverHost);
 		
 		btnMoverVM = new JButton("VM");
-		btnMoverVM.setBounds(294, 116, 89, 23);
+		btnMoverVM.setEnabled(false);
+		btnMoverVM.setBounds(292, 141, 89, 23);
 		contentPane.add(btnMoverVM);
 		
 		btnMoverProcesso = new JButton("Processo");
-		btnMoverProcesso.setBounds(294, 150, 89, 23);
+		btnMoverProcesso.setEnabled(false);
+		btnMoverProcesso.setBounds(292, 175, 89, 23);
 		contentPane.add(btnMoverProcesso);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(10, 203, 414, 247);
+		scrollPane.setBounds(10, 228, 414, 247);
 		contentPane.add(scrollPane);
 		
 		textArea = new JTextArea();
 		textArea.setLineWrap(true);
 		scrollPane.setViewportView(textArea);
+		
+		lblLog = new JLabel("");
+		lblLog.setForeground(Color.MAGENTA);
+		lblLog.setBounds(10, 41, 414, 20);
+		contentPane.add(lblLog);
+		
+		JLabel lblNewLabel = new JLabel("Espa\u00E7o de Tuplas");
+		lblNewLabel.setForeground(Color.BLUE);
+		lblNewLabel.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+		lblNewLabel.setBounds(10, 11, 133, 19);
+		contentPane.add(lblNewLabel);
 	}
 	
 	private void initActions() {
-		btnNewCloud.addActionListener(new ActionListener() {
+		btnNewNuvem.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				btnNewCloudActionPerformed(e);
 			}
@@ -203,24 +240,69 @@ public class Tela extends JFrame {
 		});
 	}
 	
+	private void instanciaEspacoTupla() {
+		try {
+			this.lblLog.setText("Procurando pelo serviço JavaSpace...");
+			Lookup finder = new Lookup(JavaSpace.class);
+			space = (JavaSpace) finder.getService();
+			
+			if(space == null) {
+				this.lblLog.setText("O serviço JavaSpace não foi encontrado. Encerrando...");
+				System.exit(-1);
+			}
+			
+			this.lblLog.setText("O serviço JavaSpace foi encontrado.");
+			System.out.println(space);
+			this.habilitaBotoes();
+			
+			/*Scanner scanner = new Scanner(System.in);
+			
+			while(true) {
+				System.out.println("Entre com o texto da mensagem (ENTER para sair): ");
+				String message = scanner.nextLine();
+				
+				if(message == null || message.equals("")) {
+					System.exit(0);
+				}
+				
+				
+			}*/
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	private void btnNewCloudActionPerformed(ActionEvent e) {
 		System.out.println("Insere Nuvem");
-		JOptionPane.showInputDialog(this, "Nome da Nuvem", "Nuvem", JOptionPane.QUESTION_MESSAGE);
+		String nomeNuvem = JOptionPane.showInputDialog(this, "Nome da Nuvem", "Nuvem", JOptionPane.QUESTION_MESSAGE);
+		try {
+			Service.createNuvem(nomeNuvem, space);
+		} catch (RemoteException | TransactionException e1) {
+			e1.printStackTrace();
+		}
 	}
 	
 	private void btnNewHostActionPerformed(ActionEvent e) {
 		System.out.println("Insere Host");
-		JOptionPane.showInputDialog(this, "Nome do Host", "Host", JOptionPane.QUESTION_MESSAGE);
+		String nomeHost = JOptionPane.showInputDialog(this, "Nome do Host", "Host", JOptionPane.QUESTION_MESSAGE);
+		try {
+			Service.createHost(nomeHost, space);
+		} catch (RemoteException | UnusableEntryException | TransactionException | InterruptedException e1) {
+			e1.printStackTrace();
+		}
 	}
 	
 	private void btnNewVMActionPerformed(ActionEvent e) {
 		System.out.println("Insere VM");
-		JOptionPane.showInputDialog(this, "Nome da VM", "VM", JOptionPane.QUESTION_MESSAGE);
+		String nomeVM = JOptionPane.showInputDialog(this, "Nome da VM", "VM", JOptionPane.QUESTION_MESSAGE);
+		Service.createVM(nomeVM, space);
 	}
 	
 	private void btnNewProcessoActionPerformed(ActionEvent e) {
 		System.out.println("Insere Processo");
-		JOptionPane.showInputDialog(this, "Nome do Processo", "Processo", JOptionPane.QUESTION_MESSAGE);
+		String nomeProcesso = JOptionPane.showInputDialog(this, "Nome do Processo", "Processo", JOptionPane.QUESTION_MESSAGE);
+		Service.createProcesso(nomeProcesso, space);
 	}
 	
 	private void btnRemoveNuvemActionPerformed(ActionEvent e) {
@@ -249,5 +331,21 @@ public class Tela extends JFrame {
 	
 	private void btnMoverProcessoActionPerformed(ActionEvent e) {
 		System.out.println("Mover Processo");
+	}
+	
+	private void habilitaBotoes() {
+		this.btnNewNuvem.setEnabled(true);
+		this.btnNewHost.setEnabled(true);
+		this.btnNewVM.setEnabled(true);
+		this.btnNewProcesso.setEnabled(true);
+		
+		this.btnRemoveNuvem.setEnabled(true);
+		this.btnRemoveHost.setEnabled(true);
+		this.btnRemoveVM.setEnabled(true);
+		this.btnRemoveProcesso.setEnabled(true);
+		
+		this.btnMoverHost.setEnabled(true);
+		this.btnMoverVM.setEnabled(true);
+		this.btnMoverProcesso.setEnabled(true);
 	}
 }
